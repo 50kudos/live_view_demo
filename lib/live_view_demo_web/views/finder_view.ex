407d5fa24388bfaa828641d370630e93d17a3f05
50kudos <50th.zaint@gmail.com>
2019-09-21 09:51:39 +0700

    Parameterize tree walking

--- lib/live_view_demo_web/views/finder_view.ex
+++ lib/live_view_demo_web/views/finder_view.ex
@@ -2,6 +2,70 @@ defmodule LiveViewDemoWeb.FinderView do
   use LiveViewDemoWeb, :view
 
   def render("tree.html", assigns) do
+    leaf_wrapper = fn ancestor_node, depth, opts ->
+      ~E"""
+        <div
+          class="<%= if(opts.selected_node == ancestor_node.id, do: 'bg-gray-700', else: 'hover:bg-gray-800') %> cursor-pointer"
+          style="padding-left: <%= 1.25 * depth %>rem"
+          phx-click="select_node"
+          phx-value-node-id="<%= ancestor_node.id %>"
+        >
+          <%= ancestor_node.key %>.<%= ancestor_node.type %>
+        </div>
+      """
+    end
+
+    node_wrapper = fn ancestor_node, subtree_view, depth, opts ->
+      ~E"""
+        <details open>
+          <summary
+            class="<%= if(opts.selected_node == ancestor_node.id, do: 'bg-gray-700', else: 'hover:bg-gray-800') %> cursor-pointer"
+            style="padding-left: <%= 1.25 * depth %>rem"
+            phx-click="select_node"
+            phx-value-node-id="<%= ancestor_node.id %>"
+          >
+            <%= ancestor_node.key %>
+          </summary>
+          <div>
+            <%= subtree_view %>
+          </div>
+        </details>
+      """
+    end
+
+    assigns =
+      assigns
+      |> put_in([:opts, :leaf_wrapper], leaf_wrapper)
+      |> put_in([:opts, :node_wrapper], node_wrapper)
+
+    ~E"""
+      <%= tree_view(@tree, 0, @opts) %>
+    """
+  end
+
+  def render("output.html", assigns) do
+    leaf_wrapper = fn ancestor_node, depth, opts ->
+      ~E"""
+        <div>
+          <%= ancestor_node.key %>.<%= ancestor_node.type %>
+        </div>
+      """
+    end
+
+    node_wrapper = fn ancestor_node, subtree_view, depth, opts ->
+      ~E"""
+        <h1><%= ancestor_node.key %>.<%= ancestor_node.type %></h1>
+        <div>
+          <%= subtree_view %>
+        </div>
+      """
+    end
+
+    assigns =
+      assigns
+      |> put_in([:opts, :leaf_wrapper], leaf_wrapper)
+      |> put_in([:opts, :node_wrapper], node_wrapper)
+
     ~E"""
       <%= tree_view(@tree, 0, @opts) %>
     """
@@ -21,16 +85,7 @@ defmodule LiveViewDemoWeb.FinderView do
 
     case children do
       [] ->
-        ~E"""
-          <div
-            class="<%= if(opts.selected_node == ancestor_node.id, do: 'bg-gray-700', else: 'hover:bg-gray-800') %> cursor-pointer"
-            style="padding-left: <%= 1.25 * depth %>rem"
-            phx-click="select_node"
-            phx-value-node-id="<%= ancestor_node.id %>"
-          >
-            <%= ancestor_node.key %>.<%= ancestor_node.type %>
-          </div>
-        """
+        opts.leaf_wrapper.(ancestor_node, depth, opts)
 
       children ->
         subtree_view =
@@ -39,21 +94,7 @@ defmodule LiveViewDemoWeb.FinderView do
             tree_view(%{tree | ancestor: descendant, paths: paths -- children}, depth + 1, opts)
           end)
 
-        ~E"""
-          <details open>
-            <summary
-              class="<%= if(opts.selected_node == ancestor_node.id, do: 'bg-gray-700', else: 'hover:bg-gray-800') %> cursor-pointer"
-              style="padding-left: <%= 1.25 * depth %>rem"
-              phx-click="select_node"
-              phx-value-node-id="<%= ancestor_node.id %>"
-            >
-              <%= ancestor_node.key %>
-            </summary>
-            <div>
-              <%= subtree_view %>
-            </div>
-          </details>
-        """
+        opts.node_wrapper.(ancestor_node, subtree_view, depth, opts)
     end
   end
 end
